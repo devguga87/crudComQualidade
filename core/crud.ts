12dbf87bf441299e4cf7422525716b137ea093d1
Gustavo Seabra <devguga87@gmail.com>
2023-10-30 12:41:20 -0300

    feat: cria metodos de update e delete

--- core/crud.ts
+++ core/crud.ts
@@ -1,14 +1,19 @@
 import fs from 'fs'
+import { v4 as uuid} from 'uuid'
 const DB_FILE_PATH = "./core/db"
 
+type UUID = string;
+
 interface Todo {
+    id:UUID;
     date: string;
     content: string;
-    done:boolean
+    done:boolean;
 }
 
 function create(content:string):Todo{
     const todo:Todo = {
+        id: uuid(),
         date: new Date().toISOString(),
         content,
         done:false
@@ -33,12 +38,50 @@ function read(): Array<Todo>{
     return db.todos
 }
 
+function update(id:UUID, partialTodo: Partial<Todo>):Todo{
+    const todos = read();
+    let updatedTodo;
+
+    todos.forEach((currentTodo)=>{
+        const isToUpdate = currentTodo.id === id
+        if(isToUpdate){
+            updatedTodo = Object.assign(currentTodo, partialTodo)
+        }
+    })
+
+    fs.writeFileSync(DB_FILE_PATH, JSON.stringify({todos},null,2))
+
+    if(!updatedTodo){
+        throw new Error("Por favor adicione um novo ID!")
+    }
+
+    return updatedTodo
+}
+
+function updateContentById(id:UUID, content:string){
+    return update(id, {content})
+}
+
+function deleteById(id:UUID){
+    const todos = read()
+    const todosWithoutOne = todos.filter(currentTodo => currentTodo.id !== id)
+
+    fs.writeFileSync(DB_FILE_PATH, JSON.stringify({todos:todosWithoutOne},null,2))
+}
+
 function DB_CLEAR(){
     fs.writeFileSync(DB_FILE_PATH,"")
 }
 
 //[SIMULATION]
 DB_CLEAR()
-create("Primeira TODO")
-create("Segunda TODO")
+const primeiraTodo = create("Primeira TODO")
+const segundaTodo = create("Segunda TODO")
+const thirdTodo = create("Terceira TODO")
+deleteById(primeiraTodo.id)
+update(segundaTodo.id , {
+    content:"Atualizada",
+    done:true
+})
+updateContentById(segundaTodo.id, "Atualizada")
 console.log(read())
