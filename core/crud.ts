8ada0f9d1449b3093c271bf943dec57893801f21
Gustavo Seabra <devguga87@gmail.com>
2024-03-14 10:18:36 -0300

    feat: cria o R do crud

--- core/crud.ts
+++ core/crud.ts
@@ -3,10 +3,42 @@ const DB_FILE_PATH = "./core/db"
 
 console.log("[CRUD]")
 
-function create(content:string){
-    fs.writeFileSync(DB_FILE_PATH, content)
-    return content;
+interface Todo {
+    date: string;
+    content: string;
+    done: boolean;
+}
+
+function create(content: string): Todo {
+    const todo = {
+        date: new Date().toISOString(),
+        content,
+        done: false
+    }
+
+    const todos: Array<Todo> = [
+        ...read(),
+        todo
+    ]
+    fs.writeFileSync(DB_FILE_PATH, JSON.stringify({ todos }, null, 2))
+    return todo;
+}
+
+function read(): Array<Todo> {
+    const dbString = fs.readFileSync(DB_FILE_PATH, "utf-8")
+    const db = JSON.parse(dbString || "{}");
+    if (!db.todos) {
+        return []
+    }
+    return db.todos
+}
+
+function CLEAR_DB() {
+    fs.writeFileSync(DB_FILE_PATH, "")
 }
 
 //[SIMULATION]
-console.log(create("First create!"))
+CLEAR_DB()
+create("First create!")
+create("Segunda Todo")
+console.log(read());
