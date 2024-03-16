1d6c59e47d32db2409d15c416f695da7a41d559f
Gustavo Seabra <devguga87@gmail.com>
2024-03-15 22:12:14 -0300

    feat: cria api das todos

---
+++ pages/api/todos.ts
@@ -0,0 +1,13 @@
+import { NextApiResponse, NextApiRequest } from "next";
+import { todoController } from "@server/controller/todo";
+
+export default function handler(
+  request: NextApiRequest,
+  response: NextApiResponse
+) {
+  if (request.method === "GET") {
+    todoController.get(request, response);
+    return;
+  }
+  response.status(405).json({ message: "Method not allowed" });
+}
