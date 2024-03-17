4e3b88a2b49a851add6c7627b37f7e870dbd1a2e
Gustavo Seabra <devguga87@gmail.com>
2024-03-17 17:32:14 -0300

    feat: trata tipagem de dado q vem da api

--- src/ui/repository/todo.ts
+++ src/ui/repository/todo.ts
@@ -15,7 +15,7 @@ function get({
 }: TodoRepositoryGetParams): Promise<TodoRepositoryGetOutput> {
   return fetch("/api/todos").then(async (respostaDoServidor) => {
     const todosString = await respostaDoServidor.text();
-    const todosFromServer = JSON.parse(todosString).todos;
+    const todosFromServer = parseTodosFromServer(JSON.parse(todosString)).todos;
 
     const ALL_TODOS = todosFromServer;
     const startIndex = (page - 1) * limit;
@@ -42,4 +42,37 @@ interface Todo {
   content: string;
   date: Date;
   done: boolean;
+}
+
+function parseTodosFromServer(responseBody: unknown): { todos: Array<Todo> } {
+  if (
+    responseBody != null &&
+    typeof responseBody === "object" &&
+    "todos" in responseBody &&
+    Array.isArray(responseBody.todos)
+  ) {
+    return {
+      todos: responseBody.todos.map((todo: unknown) => {
+        if (todo === null && typeof todo != "object") {
+          throw new Error("Invalid todo from API");
+        }
+        const { id, content, done, date } = todo as {
+          id: string;
+          content: string;
+          done: boolean;
+          date: string;
+        };
+
+        return {
+          id,
+          content,
+          done: String(done).toLocaleLowerCase() === "true",
+          date: new Date(date),
+        };
+      }),
+    };
+  }
+  return {
+    todos: [],
+  };
 }
